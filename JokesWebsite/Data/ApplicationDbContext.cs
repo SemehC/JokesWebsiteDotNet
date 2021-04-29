05f73341cc4f553720e6907fc3eaa527c0b94b11
SemehC <62266399+SemehC@users.noreply.github.com>
2021-04-29 17:37:15 +0100

    Add project files.

---
+++ JokesWebsite/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JokesWebsite.Models;
+
+namespace JokesWebsite.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWebsite.Models.Joke> Joke { get; set; }
+    }
+}
